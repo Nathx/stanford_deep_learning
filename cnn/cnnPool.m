ae7f101bd418159ed325daa358368200b90422dd
Nathan Kiner <nathankiner@gmail.com>
2016-01-15 11:20:30 +0100

    deep learning files

---
+++ cnn/cnnPool.m
@@ -0,0 +1,44 @@
+function pooledFeatures = cnnPool(poolDim, convolvedFeatures)
+%cnnPool Pools the given convolved features
+%
+% Parameters:
+%  poolDim - dimension of pooling region
+%  convolvedFeatures - convolved features to pool (as given by cnnConvolve)
+%                      convolvedFeatures(imageRow, imageCol, featureNum, imageNum)
+%
+% Returns:
+%  pooledFeatures - matrix of pooled features in the form
+%                   pooledFeatures(poolRow, poolCol, featureNum, imageNum)
+%     
+
+numImages = size(convolvedFeatures, 4);
+numFilters = size(convolvedFeatures, 3);
+convolvedDim = size(convolvedFeatures, 1);
+
+pooledFeatures = zeros(convolvedDim / poolDim, ...
+        convolvedDim / poolDim, numFilters, numImages);
+
+% Instructions:
+%   Now pool the convolved features in regions of poolDim x poolDim,
+%   to obtain the 
+%   (convolvedDim/poolDim) x (convolvedDim/poolDim) x numFeatures x numImages 
+%   matrix pooledFeatures, such that
+%   pooledFeatures(poolRow, poolCol, featureNum, imageNum) is the 
+%   value of the featureNum feature for the imageNum image pooled over the
+%   corresponding (poolRow, poolCol) pooling region. 
+%   
+%   Use mean pooling here.
+for filterNum=1:numFilters
+  for imageNum=1:numImages
+
+    for i=1:convolvedDim/poolDim
+      for j=1:convolvedDim/poolDim
+        pooledFeatures(i,j,filterNum,imageNum) = mean(mean(convolvedFeatures(((i-1)*poolDim+1):i*poolDim, ...
+        ((j-1)*poolDim+1):j*poolDim,filterNum,imageNum)));
+      end
+    end
+  end
+end
+
+end
+
---
+++ ex1/logistic_regression.m
@@ -0,0 +1,35 @@
+function [f,g] = logistic_regression(theta, X,y)
+  %
+  % Arguments:
+  %   theta - A column vector containing the parameter values to optimize.
+  %   X - The examples stored in a matrix.  
+  %       X(i,j) is the i'th coordinate of the j'th example.
+  %   y - The label for each example.  y(j) is the j'th example's label.
+  %
+
+  m=size(X,2);
+  
+  % initialize objective value and gradient.
+  f = 0;
+  g = zeros(size(theta));
+  
+  for i=1..m,
+    sig = 1/(1+exp(theta'*X(:,i))
+    f += -(y(i)*log(sig) + (1-y(i))*log(1-sig))
+ 
+    for j=1..size(g,1),
+      g(j) += X(j,i)*(sig - y(i))
+    end 
+  end
+  
+end
+
+
+  %
+  % TODO:  Compute the objective function by looping over the dataset and summing
+  %        up the objective values for each example.  Store the result in 'f'.
+  %
+  % TODO:  Compute the gradient of the objective by looping over the dataset and summing
+  %        up the gradients (df/dtheta) for each example. Store the result in 'g'.
+  %
+%%% YOUR CODE HERE %%%
